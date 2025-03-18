70ed055831f80ac043acff87aa7ede50faac60ab
NTPhus <phu444080@gmail.com>
2025-03-18 08:43:29 +0700

    cài đặt pug

--- index.ts
+++ index.ts
@@ -1,1 +1,15 @@
-console.log("OK")
+import express, { Express, Request, Response } from 'express';
+
+const app: Express = express();
+const port: number = 3000;
+
+app.set("views", "./views");
+app.set("view engine", "pug");
+
+app.get("/topic", (req: Request, res: Response) => {
+    res.render("client/pages/topics/index");
+});
+
+app.listen(port, () => {
+    console.log("App listening on port "+ port);
+})
