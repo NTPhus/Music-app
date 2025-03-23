5e1f7d8820d007f261f45deff56f660cc6c82517
NTPhus <phu444080@gmail.com>
2025-03-23 21:40:08 +0700

    tìm kiếm hiển thị suggest

--- public/js/script.js
+++ public/js/script.js
@@ -86,3 +86,47 @@ if (listButtonFavourite.length > 0) {
   });
 }
 //End Button like
+
+//Search Suggest 
+const boxSearch = document.querySelector(".box-search");
+if(boxSearch){
+  const input = boxSearch.querySelector("input[name='keyword']");
+  const boxSuggest = boxSearch.querySelector(".inner-suggest");
+
+  input.addEventListener("keyup", () => {
+    const keyword = input.value;
+    console.log(keyword);
+
+    const link = `search/suggest?keyword=${keyword}`;
+
+    fetch(link)
+      .then(res => res.json())
+      .then(data => {
+        const songs = data.songs;
+        if(songs.length > 0) {
+          boxSuggest.classList.add("show");
+
+          const html = songs.map(song => {
+            return `
+              <a href="songs/detail/${song.slug}" class="inner-item">
+                <div class="inner-image"> 
+                  <img src=${song.avatar}/>
+                </div>
+                <div class="inner-info">
+                  <div class="inner-title"> ${song.title} </div>
+                  <div class="inner-singer">
+                    <i class="fa-solid fa-microphone-lines"></i> ${song.infoSinger.fullName}  </div>
+                 </div>
+              </a>
+            `
+          });
+
+          const boxList = boxSuggest.querySelector(".inner-list");
+          boxList.innerHTML = html.join("");
+        }else{
+          boxSuggest.classList.remove("show");
+        }
+      })
+  });
+}
+//End Search Suggest 
