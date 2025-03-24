62e7bc6e78987cf523e9f2299f9faf4b2f1b10c3
NTPhus <phu444080@gmail.com>
2025-03-24 21:18:33 +0700

    Làm trang chủ đề bài hát

--- routes/admin/index.route.ts
+++ routes/admin/index.route.ts
@@ -1,12 +1,15 @@
 import { Express } from "express"
 import { dashBoardRoutes } from "./dashboard.route";
 import { systemConfig } from "../../configs/config";
+import { topicRoutes } from "./topic.route";
 
 const adminRoutes = (app: Express):void => {
 
     const pathAdmin = `/${systemConfig.prefix_admin}`
 
     app.use(pathAdmin + "/dashboard", dashBoardRoutes);
+
+    app.use(pathAdmin + "/topics", topicRoutes);
 
 }
 
