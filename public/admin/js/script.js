2e0ca2e6216b3399c69125b3f54dabb8c70cbd3c
NTPhus <phu444080@gmail.com>
2025-03-25 15:46:39 +0700

    Tính năng nghe thử audio

--- public/admin/js/script.js
+++ public/admin/js/script.js
@@ -23,3 +23,21 @@ if (uploadImage) {
   });
 }
 // End Upload Image
+
+
+// Upload audio
+const uploadAudio = document.querySelector("[upload-audio]");
+if (uploadAudio) {
+  const uploadAudioInput = document.querySelector("[upload-audio-input]");
+  const uploadAudioPlay = document.querySelector("[upload-audio-play]");
+  const source = uploadAudio.querySelector("source");
+
+  uploadAudioInput.addEventListener("change", (e) => {
+    const file = e.target.files[0];
+    if (file) {
+      source.src = URL.createObjectURL(file);
+      uploadAudioPlay.load();
+    }
+  });
+}
+// End Upload Audio
