9946ea14777589ee12dfa2478eb782de669db95e
NTPhus <phu444080@gmail.com>
2025-03-25 15:40:54 +0700

    upload file audio lên cloud

--- middlewares/admin/uploadCloud.middleware.ts
+++ middlewares/admin/uploadCloud.middleware.ts
@@ -1,50 +1,45 @@
 import { Request, Response, NextFunction } from "express";
-import {v2 as cloudinary} from "cloudinary";
-import streamifier from "streamifier";
-import dotenv from "dotenv";
-
-dotenv.config();
-//Cloudirady
-cloudinary.config({
-  cloud_name: process.env.CLOUD_NAME,
-  api_key: process.env.CLOUD_KEY,
-  api_secret: process.env.CLOUD_SECRET, // Click 'View API Keys' above to copy your API secret
-});
-//End Cloudirady
-
-const streamUpload = (buffer: any) => {
-  return new Promise((resolve, reject) => {
-    let stream = cloudinary.uploader.upload_stream({ resource_type: "auto"},(error, result) => {
-      if (result) {
-        resolve(result);
-      } else {
-        reject(error);
-      }
-    });
-
-    streamifier.createReadStream(buffer).pipe(stream);
-  });
-};
-
-const uploadToCloudinary = async (buffer: any) => {
-  let result:any = await streamUpload(buffer);
-  return result.secure_url;
-};
+import { uploadToCloudinary } from "../../helpers/uploadToClouldinary";
 
 export const uploadSingle = async (req: Request, res: Response, next: NextFunction) => {
   try {
-      if (!req.file) {
-          throw new Error("No file provided");
-      }
+    if (!req.file) {
+      throw new Error("No file provided");
+    }
 
-      const result = await uploadToCloudinary(req.file.buffer);
-      if (req.file.fieldname) {
-          req.body[req.file.fieldname] = result;
-      }
+    const result = await uploadToCloudinary(req.file.buffer);
+    if (req.file.fieldname) {
+      req.body[req.file.fieldname] = result;
+    }
   } catch (error) {
-      console.error("Upload error:", error);
+    console.error("Upload error:", error);
   }
 
   next();
 };
 
+export const uploadFields = async (req: Request, res: Response, next: NextFunction): Promise<void> => {
+  if (!req.files) {
+    console.error("req.files is undefined.");
+    next();
+  } else if (Array.isArray(req.files)) {
+    console.error("req.files is a File array, not an object with keys.");
+    next();
+  } else {
+    for (const key in req.files) {
+      req.body[key] = [];
+
+      const array = req.files[key];
+      for (const item of array) {
+        try {
+          const result = await uploadToCloudinary(item.buffer);
+          req.body[key].push(result);
+        } catch (error) {
+          console.error(error);
+        }
+      }
+    }
+
+    next();
+  }
+};
